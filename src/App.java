eb4df9f9d55714dd471a7c159b1536722a8c301b
ibmc <ibmedcentre@gmail.com>
2026-04-19 22:14:54 +0100

    Refactor App class to restore hardcoded credentials and update greeting message

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Issue: Hardcoded credentials (should be flagged in code review)
-        //String username = "admin";
-        //String password = "P@ssw0rd123";
+        String username = "admin";
+        String password = "P@ssw0rd123";
+
         if (args.length > 0) {
             System.out.println("Arguments:");
             for (String arg : args) {
@@ -11,6 +12,6 @@ public class App {
         } else {
             System.out.println("No arguments provided.");
         }
-        System.out.println("Hello, World 2222!");
+        System.out.println("Hello, World " + username + "!");
     }
 }
