bfd06513de7a232e9740de478eddaee1903d45ae
ibmc <ibmedcentre@gmail.com>
2026-04-19 17:29:44 +0100

    Enhance App class to handle command-line arguments and provide feedback

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            System.out.println("Arguments:");
+            for (String arg : args) {
+                System.out.println(arg);
+            }
+        } else {
+            System.out.println("No arguments provided.");
+        }
         System.out.println("Hello, World!");
     }
 }
